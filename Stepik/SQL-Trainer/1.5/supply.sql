ddbacd9e8629284b6d89a99d89010b75b6a30c90
Sergey Ocheretenko <ocheretenko.s@gmail.com>
2021-10-15 21:42:18 +0300

    Add INSERT INTO supply

--- Stepik/SQL-Trainer/1.5/supply.sql
+++ Stepik/SQL-Trainer/1.5/supply.sql
@@ -5,3 +5,9 @@ CREATE TABLE supply(
     price DECIMAL(8, 2),
     amount INT
 );
+
+INSERT INTO supply(title, author, price, amount)
+VALUES ('Лирика', 'Пастернак Б.Л.', 518.99, 2), 
+    ('Черный человек', 'Есенин С.А.', 570.20, 6),
+    ('Белая гвардия', 'Булгаков М.А.', 540.50, 7),
+    ('Идиот','Достоевский Ф.М.', 360.80, 3);
