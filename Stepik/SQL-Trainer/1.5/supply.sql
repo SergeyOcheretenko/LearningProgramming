de8f4265d8089d0cdae2070862147867d9fd2597
Sergey Ocheretenko <ocheretenko.s@gmail.com>
2021-10-17 23:24:36 +0300

    Add INSERT INTO book FROM supply

--- Stepik/SQL-Trainer/1.5/supply.sql
+++ Stepik/SQL-Trainer/1.5/supply.sql
@@ -11,3 +11,8 @@ VALUES ('Лирика', 'Пастернак Б.Л.', 518.99, 2),
     ('Черный человек', 'Есенин С.А.', 570.20, 6),
     ('Белая гвардия', 'Булгаков М.А.', 540.50, 7),
     ('Идиот','Достоевский Ф.М.', 360.80, 3);
+
+INSERT INTO book(title, author, price, amount)
+SELECT title, author, price, amount
+FROM supply 
+WHERE author <> 'Булгаков М.А.' AND author <> 'Достоевский Ф.М.';
