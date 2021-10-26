8cd53970294f8c29188dbcac946f4f13a4ece584
Sergey Ocheretenko <ocheretenko.s@gmail.com>
2021-10-26 23:42:04 +0300

    Add INSERT INTO book 3 new values

--- Stepik/SQL-Trainer/1.1_TABLE/book.sql
+++ Stepik/SQL-Trainer/1.1_TABLE/book.sql
@@ -8,3 +8,8 @@ CREATE TABLE book(
 
 INSERT INTO book(title, author, price, amount)
 VALUES ('Мастер и Маргарита', 'Булгаков М.А.', 670.99, 3);
+
+INSERT INTO book(title, author, price, amount)
+VALUES ('Белая гвардия', 'Булгаков М.А.', 540.50, 5), 
+('Идиот', 'Достоевский Ф.М.', 460, 10),
+('Братья Карамазовы', 'Достоевский Ф.М.', 799.01, 2);
