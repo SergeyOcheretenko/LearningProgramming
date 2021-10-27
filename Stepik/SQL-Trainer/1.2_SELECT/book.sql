403a3fdfdb2057679f5635fb7de0b18d19b69ec4
Sergey Ocheretenko <ocheretenko.s@gmail.com>
2021-10-27 22:45:07 +0300

    Add IF and IF in IF

--- Stepik/SQL-Trainer/1.2_SELECT/book.sql
+++ Stepik/SQL-Trainer/1.2_SELECT/book.sql
@@ -25,3 +25,7 @@ from book;
 
 SELECT title, author, amount, ROUND(price * 0.7, 2) AS new_price
 FROM book;
+
+SELECT author, title, 
+    ROUND(IF(author = 'Булгаков М.А.', price * 1.1, IF(author = 'Есенин С.А.', price * 1.05, price)), 2) as new_price
+FROM book;
