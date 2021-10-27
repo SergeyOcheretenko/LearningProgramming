95abea961e89e3379aa7128786476f340d6261f3
Sergey Ocheretenko <ocheretenko.s@gmail.com>
2021-10-27 22:46:17 +0300

    Add BETWEEN, AND, IN

--- Stepik/SQL-Trainer/1.2_SELECT/book.sql
+++ Stepik/SQL-Trainer/1.2_SELECT/book.sql
@@ -37,3 +37,7 @@ WHERE amount < 10;
 SELECT title, author, price, amount
 FROM book
 WHERE (price < 500 OR price > 600) AND price * amount >= 5000;
+
+SELECT title, author
+FROM book
+WHERE (price BETWEEN 540.50 AND 800) AND amount IN (2, 3, 5, 7);
