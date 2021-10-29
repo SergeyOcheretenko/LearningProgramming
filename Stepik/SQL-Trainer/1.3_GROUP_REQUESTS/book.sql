41fd16d1d6f6ea9ad6235521e31e49a397ef684e
Sergey Ocheretenko <ocheretenko.s@gmail.com>
2021-10-29 23:00:45 +0300

    Add MIN, MAX, AVG

--- Stepik/SQL-Trainer/1.3_GROUP_REQUESTS/book.sql
+++ Stepik/SQL-Trainer/1.3_GROUP_REQUESTS/book.sql
@@ -19,3 +19,7 @@ FROM book;
 
 SELECT DISTINCT amount
 FROM book;
+
+SELECT author, MIN(price) AS Минимальная_цена, MAX(price) AS Максимальная_цена, AVG(price) AS Средняя_цена
+FROM book
+GROUP BY author;
