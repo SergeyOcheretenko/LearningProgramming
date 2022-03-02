ea210689609bfd3d7bf21bde084d6feb0eece7f2
SergeyOcheretenko <ocheretenko.s@gmail.com>
2022-03-02 07:57:06 +0200

    fix: delete old folder

--- Go/Stepik/Golang_Programming/Basics/Exercises/14-binary.go
+++
@@ -1,9 +0,0 @@
-package main
-
-import "fmt"
-
-func main() {
-    var number int
-    fmt.Scan(&number)
-    fmt.Printf("%b\n", number)
-}
--- Go/Stepik/Golang_Programming/Basics/Exercises/2-reverse.go
+++
@@ -1,9 +0,0 @@
-package main
-
-import "fmt"
-
-func main() {
-    var number string
-    fmt.Scan(&number)
-    fmt.Println(string(number[2]) + string(number[1]) + string(number[0]))
-}
