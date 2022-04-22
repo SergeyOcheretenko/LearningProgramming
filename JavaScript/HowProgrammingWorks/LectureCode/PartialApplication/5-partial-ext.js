a207976f2d7119917d5964f1ef7006dd2864297c
Sergey Ocheretenko <ocheretenko.s@gmail.com>
2022-04-22 17:01:08 +0300

    feat: add partial usage examples

---
+++ JavaScript/HowProgrammingWorks/LectureCode/PartialApplication/5-partial-ext.js
@@ -0,0 +1,17 @@
+'use strict';
+
+const partial = (fn, ...args) => (...rest) => fn(...args.concat(rest));
+
+// Usage
+
+const sum4 = (a, b, c, d) => (a + b + c + d);
+
+const f11 = partial(sum4, 1);
+const f12 = partial(f11, 2);
+const f13 = partial(f12, 3);
+const y1 = f13(4);
+console.log(y1);
+
+const f21 = partial(sum4, 1, 2);
+const y2 = f21(3, 4);
+console.log(y2);
