90a5940f7143efbef97720483cbac4a8570c26a8
Sergey Ocheretenko <ocheretenko.s@gmail.com>
2022-06-24 19:10:41 +0300

    Fix NodeJS course structure

---
+++ JavaScript/Courses/Udemy/NodeJS/7-Http/1-server.js
@@ -0,0 +1,14 @@
+'use strict';
+
+const host = '127.0.0.1';
+const port = 8000;
+
+const server = http.createServer((req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Hello!')
+});
+
+server.listne(port, host, () => {
+    console.log('Started');
+});
